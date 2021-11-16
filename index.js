aad8ce69f1c4c7a46169f5d21479bb321d5fe7dc
Marisa NeSmith <marisanesmith@Marisas-MacBook-Pro.local>
2021-11-15 17:59:40 -0700

    added photos and adjusted padding around elements

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ $('p').scroll(function(){
     $('introScroll').fadeIn(2000);
 })
 
-$(document).ready(function() {
-    $('.hamburger-icon').on('click', function() {
-        $('.animated-icon').toggleClass('open');
-    });
-})
+// $(document).ready(function() {
+//     $('.hamburger-icon').on('click', function() {
+//         $('.animated-icon').toggleClass('open');
+//     });
+// })
+
+
+// Typed JS Code
+
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 10,
+    backSpeed: 5,
+    smartBackspace: true,
+    loop: true,
+    loopCount: Infinity
+  });
